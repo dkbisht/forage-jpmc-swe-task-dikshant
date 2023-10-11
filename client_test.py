884b9a5db7929fd20d601bc4a56870eaf64637c2
Dikshant <dikshantbisht02@gmail.com>
2023-10-11 16:00:38 +0530

    Unit testing Done Commit

--- client_test.py
+++ client_test.py
@@ -1,6 +1,6 @@
 import unittest
 from client3 import getDataPoint
-
+from client3 import getRatio
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
     quotes = [
@@ -8,17 +8,35 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], ( quote['top_bid']['price'] + quote['top_ask']['price'])/2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], ( quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    price_a =110
+    price_b=100
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(price_a, price_b), (price_a/price_b))
 
+  def test_getRatio_calculateRatioNegative(self):
+    price_a = -110
+    price_b = 100
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(price_a, price_b), (price_a / price_b))
+
+  def test_getRatio_calculateRatioZero(self):
+    price_a = 10
+    price_b = 0
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(price_a, price_b), None)
 
 
 if __name__ == '__main__':
